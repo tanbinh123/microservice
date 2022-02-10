1e7939562624b98277f25f9d1cc2536fac3e2e5b
MrRiver <411833172@qq.com>
2022-02-10 14:13:49 +0800

    Delete src directory

--- src/main/webapp/app/scripts/controllers/tools/performanceMonitor.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-angular.module('webApp').controller('PerformanceMonitorCtrl',function ($scope,$state,$http,$stateParams,Upload) {
-	$scope.druidSrc = '../druid';
-});
--- src/main/webapp/app/scripts/directives/header/header.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-angular.module('webApp')
-	   .directive('header',function(){
-		   return {
-			   templateUrl:'scripts/directives/header/header.html',
-			   restrict: 'E',
-			   replace: true,
-		   }
-	   });
