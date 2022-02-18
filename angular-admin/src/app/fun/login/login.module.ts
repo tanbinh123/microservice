12b7597aaeca3af032d3a6f2b4662e3334432d53
MrRiver <411833172@qq.com>
2022-02-18 16:25:49 +0800

    Update login.module.ts

--- angular-admin/src/app/fun/login/login.module.ts
+++ angular-admin/src/app/fun/login/login.module.ts
@@ -1,12 +1,13 @@
-import {CommonModule} from '@angular/common';
-import {FormsModule} from '@angular/forms';
-import {NgModule} from '@angular/core';
-import {RouterModule} from '@angular/router';
-import {LoginComponent} from './login.components';
-import {LoginKeyDownDirective} from 'src/app/directive/LoginKeyDownDirective';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { LoginComponent } from './login.components';
+import { LoginKeyDownDirective } from 'src/app/directive/LoginKeyDownDirective';
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
-  imports:[CommonModule,FormsModule,RouterModule],
+  imports:[CommonModule,FormsModule,RouterModule,ButtonModule],
   declarations:[LoginComponent,LoginKeyDownDirective],
   exports:[LoginComponent]
 })
