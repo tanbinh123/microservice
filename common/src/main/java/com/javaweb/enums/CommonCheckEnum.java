0939285ce43a6c0d4733be666df7e773a24a2f15
MrRiver <411833172@qq.com>
2022-02-22 14:13:07 +0800

    Update CommonCheckEnum.java

--- common/src/main/java/com/javaweb/enums/CommonCheckEnum.java
+++ common/src/main/java/com/javaweb/enums/CommonCheckEnum.java
@@ -2,12 +2,14 @@ package com.javaweb.enums;
 
 public enum CommonCheckEnum {
 
+	CAR_NO,//车牌号
+
 	ID_CARD,//身份证
 	
-	USCC,//统一社会信用代码
-	
-	CAR_NO,//车牌号
-	
-	PHONE_NO//手机号
+	IP,//IP地址
+
+	PHONE_NO,//手机号
+
+	USCC//统一社会信用代码
 
 }
