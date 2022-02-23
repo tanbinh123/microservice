2123193abc95457d5865f513602cd1b62b301b60
MrRiver <411833172@qq.com>
2022-02-23 08:58:19 +0800

    Update and rename AddResponseHeaderFilter.java to ResponseHeaderFilter.java

--- eureka-client-admin/src/main/java/com/javaweb/filter/ResponseHeaderFilter.java
+++ eureka-client-admin/src/main/java/com/javaweb/filter/ResponseHeaderFilter.java
@@ -13,7 +13,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import com.javaweb.base.BaseInject;
 
 @Component
-public class AddResponseHeaderFilter extends OncePerRequestFilter {
+public class ResponseHeaderFilter extends OncePerRequestFilter {
 	
 	//x.y.z：x为主版本号；y为次版本号（功能）；z为修订号（bug修复）
 	protected void doFilterInternal(HttpServletRequest request,HttpServletResponse response,FilterChain filterChain) throws ServletException,IOException {
