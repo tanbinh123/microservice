c8f2af6c790a7003cd0be383cd5968686d008332
MrRiver <411833172@qq.com>
2022-03-01 11:54:52 +0800

    Update sidebar.module.ts

--- angular-admin/src/app/fun/home/sidebar/sidebar.module.ts
+++ angular-admin/src/app/fun/home/sidebar/sidebar.module.ts
@@ -1,12 +1,12 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {RouterModule} from '@angular/router';
-import {SidebarComponent} from './sidebar.component';
-import {SidebarRecursionComponent} from './sidebar.recursion.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { SidebarComponent } from './sidebar.component';
+import { PanelMenuModule } from "primeng/panelmenu";
 
 @NgModule({
-  imports:[CommonModule,RouterModule],
-  declarations:[SidebarComponent,SidebarRecursionComponent],
+  imports:[CommonModule,RouterModule,PanelMenuModule],
+  declarations:[SidebarComponent],
   exports:[SidebarComponent]
 })
 
