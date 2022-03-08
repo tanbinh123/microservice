1a481a52ae2e56b3c519a92aa2793f0d9ee70d71
MrRiver <411833172@qq.com>
2022-03-08 09:38:43 +0800

    Update role.detail.module.ts

--- angular-admin/src/app/fun/role/detail/role.detail.module.ts
+++ angular-admin/src/app/fun/role/detail/role.detail.module.ts
@@ -1,14 +1,17 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {RouterModule} from '@angular/router';
-import {FormsModule} from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
-import {RoleDetailComponent} from './role.detail.component';
+import { RoleDetailComponent } from './role.detail.component';
+import { TableModule } from "primeng/table";
+import { InputTextModule } from "primeng/inputtext";
+import { ButtonModule } from "primeng/button";
 
 @NgModule({
-  imports: [CommonModule, RouterModule, FormsModule],
-    declarations:[RoleDetailComponent],
-    exports:[RoleDetailComponent]
+  imports: [CommonModule, RouterModule, FormsModule, TableModule, InputTextModule, ButtonModule],
+  declarations:[RoleDetailComponent],
+  exports:[RoleDetailComponent]
 })
 
 export class RoleDetailModule {
