84277807b71f4911aa55811c563484b4f6fa00f6
MrRiver <411833172@qq.com>
2022-03-08 09:33:46 +0800

    Update role.modify.module.ts

--- angular-admin/src/app/fun/role/modify/role.modify.module.ts
+++ angular-admin/src/app/fun/role/modify/role.modify.module.ts
@@ -1,14 +1,17 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {RouterModule} from '@angular/router';
-import {FormsModule} from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
-import {RoleModifyComponent} from './role.modify.component';
+import { RoleModifyComponent } from './role.modify.component';
+import { TableModule } from "primeng/table";
+import { InputTextModule } from "primeng/inputtext";
+import { ButtonModule } from "primeng/button";
 
 @NgModule({
-  imports: [CommonModule, RouterModule, FormsModule],
-    declarations:[RoleModifyComponent],
-    exports:[RoleModifyComponent]
+  imports: [CommonModule, RouterModule, FormsModule, TableModule, InputTextModule, ButtonModule],
+  declarations:[RoleModifyComponent],
+  exports:[RoleModifyComponent]
 })
 
 export class RoleModifyModule {
