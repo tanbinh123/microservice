62136b59d91967616d3822753a23db55aa96ee71
MrRiver <411833172@qq.com>
2022-03-08 09:48:40 +0800

    Update role.add.module.ts

--- angular-admin/src/app/fun/role/add/role.add.module.ts
+++ angular-admin/src/app/fun/role/add/role.add.module.ts
@@ -1,14 +1,17 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {RouterModule} from '@angular/router';
-import {FormsModule} from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
-import {RoleAddComponent} from './role.add.component';
+import { RoleAddComponent } from './role.add.component';
+import { TableModule } from "primeng/table";
+import { InputTextModule } from "primeng/inputtext";
+import { ButtonModule } from "primeng/button";
 
 @NgModule({
-  imports: [CommonModule, RouterModule, FormsModule],
-    declarations:[RoleAddComponent],
-    exports:[RoleAddComponent]
+  imports: [CommonModule, RouterModule, FormsModule, TableModule, InputTextModule, ButtonModule],
+  declarations:[RoleAddComponent],
+  exports:[RoleAddComponent]
 })
 
 export class RoleAddModule {
