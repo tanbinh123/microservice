505afeb8a1318d2ecc85a6266de836dc6dbc9529
MrRiver <411833172@qq.com>
2022-03-16 17:05:35 +0800

    Add files via upload

--- angular-admin/src/app/fun/role/roleModuleAssignment/role.roleModuleAssignment.component.ts
+++ angular-admin/src/app/fun/role/roleModuleAssignment/role.roleModuleAssignment.component.ts
@@ -1,11 +1,13 @@
-import {Component,OnInit} from '@angular/core';
-import {Router, ActivatedRoute} from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 
-import {HttpService} from '../../../service/HttpService';
-import {AuthService} from '../../../service/AuthService';
-import {SessionService} from '../../../service/SessionService';
-import {ApiConstant} from '../../../constant/ApiConstant';
-import {RoleModuleAssignmentModuleRecursionComponent} from './role.roleModuleAssignment.module.recursion.component';
+import { HttpService } from '../../../service/HttpService';
+import { AuthService } from '../../../service/AuthService';
+import { SessionService } from '../../../service/SessionService';
+import { ApiConstant } from '../../../constant/ApiConstant';
+import { NzFormatEmitEvent } from 'ng-zorro-antd/tree';
+import { NzMessageService } from 'ng-zorro-antd/message';
+import { finalize } from 'rxjs';
 
 @Component({
   selector: 'app-web-role-role-module-assignment',
@@ -20,17 +22,21 @@ export class RoleModuleAssignmentComponent implements OnInit {
               public activatedRoute:ActivatedRoute,
               public httpService:HttpService,
               public authService:AuthService,
-              public sessionService:SessionService){
-
+              public sessionService:SessionService,
+              public nzMessageService:NzMessageService){
+    //do nothing
   }
-
-  public needRoleId:string;
-
-  public moduleTree:any;
 
   //引入子组件（若使用子组件中的静态变量则不需要引入）
   //@ViewChild('userModuleAssignmentModuleRecursionComponent',{static:false})
-  //public userModuleAssignmentModuleRecursionComponent:UserModuleAssignmentModuleRecursionComponent;
+  //userModuleAssignmentModuleRecursionComponent:UserModuleAssignmentModuleRecursionComponent;
+
+  loading = false;//数据加载
+  needRoleId:string;
+  moduleTree:any;
+  idList:Array<string> = new Array<string>();
+  checkedKeys:Array<string> = [];
+  nzCheckBoxChangeEvent:NzFormatEmitEvent;
 
   //初始化
   ngOnInit(): void {
@@ -48,42 +54,135 @@ export class RoleModuleAssignmentComponent implements OnInit {
         next:(result:any) => {
           //console.log(result);
           if(result.code==200){
-            this.moduleTree = result.data;
+            this.idList = result.data.filter(e=>e.checkFlag==true).map(e=>e.moduleId);//数据库原始数据
+            this.moduleTree = this.setTreeList(result.data,null);//组件的树形结构封装
+            /** 真正的checkedKeys集合 start */
+            this.checkedKeys = result.data.filter(e => (e.checkFlag==true)&&(e.level==3)).map(e=>e.moduleId);//勾选的checkbox
+            this.specHandleCheckedKeys(this.moduleTree);//特殊处理，递归获取级别是2（菜单）的且没有子节点的
+            /** 真正的checkedKeys集合 end */
           }else{
-            alert(result.message);
+            this.nzMessageService.error(result.message);
           }
         },
-        error:e => {},
-        complete:() => {}
+        error:e => {
+          this.nzMessageService.error(e.message);
+        },
+        complete:() => {
+          //do nothing
+        }
       }
     );
   }
 
+  //checkbox勾选事件
+  public nzCheckBoxChange(event: NzFormatEmitEvent){
+    this.nzCheckBoxChangeEvent = event;
+  }
+
   //角色模块分配
   public roleModuleAssignment():void{
-    //console.log(UserModuleAssignmentModuleRecursionComponent.idList);
-    this.httpService.requestJsonData(ApiConstant.SYS_ROLE_MODULE_ASSIGNMENT,JSON.stringify(RoleModuleAssignmentModuleRecursionComponent.idList),new Map([['roleId',this.needRoleId]])).subscribe(
+    this.getCheckedIds();
+    this.httpService.requestJsonData(ApiConstant.SYS_ROLE_MODULE_ASSIGNMENT,JSON.stringify(this.idList),new Map([['roleId',this.needRoleId]]))
+      .pipe(finalize(()=>this.loading=false)).subscribe(
       {
         next:(result:any) => {
           //console.log(result);
           if(result.code==200){
-            alert('角色模块分配成功');
-            RoleModuleAssignmentModuleRecursionComponent.idList = [];//分配成功清空子组件中静态变量的值
+            this.nzMessageService.success('角色模块分配成功');
             this.router.navigate(['../'],{relativeTo:this.activatedRoute});
           }else{
-            alert(result.message);
+            this.nzMessageService.error(result.message);
           }
         },
-        error:e => {},
-        complete:() => {}
+        error:e => {
+          this.nzMessageService.error(e.message);
+        },
+        complete:() => {
+          //do nothing
+        }
       }
     );
   }
 
   //返回
   public roleModuleAssignmentBack():void{
-    RoleModuleAssignmentModuleRecursionComponent.idList = [];//点击返回清空子组件中静态变量的值
     this.router.navigate(['../'],{relativeTo:this.activatedRoute});
+  }
+
+  private specHandleCheckedKeys(moduleTree:any):void {
+    if(moduleTree!=null&&moduleTree.length>0){
+      for(let i=0;i<moduleTree.length;i++){
+        let each = moduleTree[i];
+        if(each.level==2&&each.seflChecked==true&&each.children==null){
+          this.checkedKeys.push(each.key);
+        }else{
+          this.specHandleCheckedKeys(each.children);
+        }
+      }
+    }
+  }
+
+  private getCheckedIds():void{
+    if(this.nzCheckBoxChangeEvent != undefined){
+      let keys = this.nzCheckBoxChangeEvent.checkedKeys;
+      this.idList = new Array<string>();
+      if(keys.length>0){
+        this.getChildrenIdList(keys);
+        for(let i=0;i<keys.length;i++){
+          this.getParentIdList(keys[i]);
+        }
+        this.idList = Array.from(new Set(this.idList));//去重
+      }
+    }
+    //console.log(this.idList);
+  }
+
+  private getChildrenIdList(keys:any):void{
+    for(let i=0;i<keys.length;i++){
+      let eachKey = keys[i];
+      if(eachKey.children==null||eachKey.children.length==0){//无子节点
+        this.idList.push(eachKey.key);
+      }else{
+        this.idList.push(eachKey.key);
+        this.getChildrenIdList(eachKey.children);
+      }
+    }
+  }
+
+  private getParentIdList(key:any):void{
+    if(key.parentNode!=null){//有父节点
+      this.idList.push(key.key);
+      this.getParentIdList(key.parentNode);
+    }else{
+      this.idList.push(key.key);
+    }
+  }
+
+  //树形结构递归封装符合NgZorro框架中Tree组件的数据格式要求（这里后端返回的moduleTree并未封装成树形，主要考虑到以后如果换前端组件，可能数据格式又是另一个了）
+  private setTreeList(moduleTree:any,eachMenu:any):any {
+    let menuList:Array<any> = [];
+    for(let i=0;i<moduleTree.length;i++) {
+      let currentEach = moduleTree[i];
+      let changeEach = {
+        'key': currentEach.moduleId,
+        'title': currentEach.moduleName,
+        'expanded': (currentEach.level == 1),
+        'level': currentEach.level,
+        'isLeaf': (currentEach.level == 3),
+        'seflChecked': currentEach.checkFlag
+      };
+      if((eachMenu!=null&&eachMenu.moduleId==currentEach.parentId)||(eachMenu==null&&currentEach.parentId==null)) {
+        changeEach['children'] = this.setTreeList(moduleTree,currentEach);
+        if(changeEach['children']==null){
+          changeEach['isLeaf'] = true;
+        }
+        menuList.push(changeEach);
+      }
+    }
+    if(menuList.length==0){
+      menuList = null;
+    }
+    return menuList;
   }
 
 }
--- angular-admin/src/app/fun/role/roleModuleAssignment/role.roleModuleAssignment.module.ts
+++ angular-admin/src/app/fun/role/roleModuleAssignment/role.roleModuleAssignment.module.ts
@@ -1,14 +1,15 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {RouterModule} from '@angular/router';
-import {FormsModule} from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
-import {RoleModuleAssignmentComponent} from './role.roleModuleAssignment.component';
-import {RoleModuleAssignmentModuleRecursionComponent} from './role.roleModuleAssignment.module.recursion.component';
+import { RoleModuleAssignmentComponent } from './role.roleModuleAssignment.component';
+import { NzTreeModule } from 'ng-zorro-antd/tree';
+import { NzButtonModule } from 'ng-zorro-antd/button';
 
 @NgModule({
-  imports: [CommonModule, RouterModule, FormsModule],
-    declarations:[RoleModuleAssignmentComponent,RoleModuleAssignmentModuleRecursionComponent],
+  imports: [CommonModule, RouterModule, FormsModule, NzTreeModule, NzButtonModule],
+    declarations:[RoleModuleAssignmentComponent],
     exports:[RoleModuleAssignmentComponent]
 })
 
